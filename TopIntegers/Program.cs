5ac05b57bf0a89daa5e9845e5b084d8765c36f55
Nikolay Borisov <niksprotips@gmail.com>
2024-10-02 16:52:12 +0300

    Rename solution

---
+++ TopIntegers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TopIntegers
+{
+    internal class Program
+    {
+        private static int i;
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] array = input.Split();
+
+            int[] integers = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int num = int.Parse(array[i]);
+                integers[i] = num;
+            }
+            //-------------------
+
+            int max = int.MinValue;
+
+            for (int j = 0; j < integers.Length; j++)
+            {
+                var current = integers[j];
+
+                bool forprint = true;
+
+                for (int k = j; k < integers.Length; k++)
+                {
+                    if (current < integers[k])
+                    {
+                        forprint = false;
+                        break;
+                    }
+                }
+                if (forprint)
+                {
+                    Console.Write(current + " ");
+                }
+                
+            }
+
+            //Console.WriteLine(max);
+            //Console.WriteLine($"index:{maxIndex}");
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string elements = "apple banana orange";
+            //string[] fruits = elements.Split();
+            //for (int i = 0; i < fruits.Length; i++)
+            //{
+            //    Console.WriteLine(fruits[i] + " ");
+            //}
+
+
+            //string[] vegitables = new string[] { "pumpkin", "tomato", "eggplant", "cucumber" };
+
+            //for (int i = 0; i < vegitables.Length; i++)
+            //{
+            //    Console.WriteLine(vegitables[i] + " ");
+            //}
+
+
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            string result = "no";
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int left = 0; left < i; left++)  // leftSum
+                {
+                    leftSum += nums[left];
+                }
+
+                for (int right = i + 1; right < nums.Length; right++)  // rightSum
+                {
+                    rightSum += nums[right];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    result = i.ToString();
+                }
+
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
